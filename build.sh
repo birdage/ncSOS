f5f00557a4ba802ff640dbd83ce729733dd189da
Andy Bird <abird@asascience.com>
2014-03-11 12:55:09 -0400

    update caps and generate build script for release version

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+echo "Building PostSOS..."
+echo ""
+echo "Directory:" 
+pwd
+echo ""
+echo "Making Zip..."
+rm ./release/postSOS-1.0.0.zip
+zip -j ./release/postSOS-1.0.0.zip ./target/postSOS-1.0.jar
+zip ./release/postSOS-1.0.0.zip ./sos_resources/*
+zip -j ./release/postSOS-1.0.0.zip ./jar/*
+echo ""
+echo "Build Complete PostSOS..."
