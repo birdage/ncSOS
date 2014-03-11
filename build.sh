4d22c4712aa5faaa5f3232306ab2d793c724a0cf
Andy Bird <abird@asascience.com>
2014-03-11 14:16:16 -0400

    update build script to include all files in a directory

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ echo ""
 echo "Making Zip..."
 rm ./release/postSOS-1.0.0.zip
 zip -j ./release/postSOS-1.0.0.zip ./target/postSOS-1.0.jar
-zip ./release/postSOS-1.0.0.zip ./sos_resources/*
+zip -r ./release/postSOS-1.0.0.zip ./sos_resources/*
 zip -j ./release/postSOS-1.0.0.zip ./jar/*
 echo ""
 echo "Build Complete PostSOS..."
